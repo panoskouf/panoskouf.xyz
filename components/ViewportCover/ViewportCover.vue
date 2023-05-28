cbbbd0c5f95939176c23e835c1e314f9a4b2b055
panoskouf <panoskouf@outlook.com>
2023-05-29 00:15:00 +0200

    setup sass, index page and add css reset

---
+++ components/ViewportCover/ViewportCover.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="viewportCoverContainer">
+    <div class="viewportCover">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+$whitespaceGap: 24px;
+.viewportCoverContainer {
+  width: 100%;
+  height: 100vh;
+  position: relative;
+}
+.viewportCover {
+  position: absolute;
+  top: $whitespaceGap;
+  left: $whitespaceGap;
+  background-color: #f9f3e7;
+  height: calc(100% - #{$whitespaceGap * 2});
+  width: calc(100% - #{$whitespaceGap * 2});
+  border-radius: 16px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<script setup>
+import ViewportCover from '../components/ViewportCover/ViewportCover.vue';
+</script>
+
+<template>
+  <ViewportCover>@todo</ViewportCover>
+</template>
