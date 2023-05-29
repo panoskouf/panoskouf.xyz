323df22b85add929cf0f6235fc67c3dfa006cdc7
panoskouf <panoskouf@outlook.com>
2023-05-29 16:34:55 +0200

    add initial Text element

---
+++ component-library/Text.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { Color, colors, getColor } from '~/styles';
+
+export type Props = {
+  is?: string;
+  // @todo implement variants
+  color?: Color;
+  align?: 'inherit' | 'left' | 'right' | 'center' | 'justify';
+};
+
+const props = withDefaults(defineProps<Props>(), {
+  // @todo should the default be 'span' or 'p'?
+  is: 'span',
+  // why TS complains here ????
+  // color: 'inherit',
+  // this is fine --> const x: Color = 'inherit';
+  align: 'inherit',
+});
+
+const _color = computed(() =>
+  props.color ? getColor(props.color) : 'inherit'
+);
+</script>
+
+<template>
+  <component :is="is" class="text"><slot /></component>
+</template>
+
+<style lang="scss" scoped>
+.text {
+  display: inline-block;
+  font-family: inherit;
+  font-style: inherit;
+  font-weight: inherit;
+  font-size: inherit;
+  line-height: inherit;
+
+  color: v-bind('_color');
+  text-align: v-bind('align');
+}
+</style>
